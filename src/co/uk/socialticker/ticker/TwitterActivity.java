bf2b1abac8e9a94ef93194e68353091913806596
captainstu72 <github@stuartalsford.me.uk>
2014-03-13 11:58:52 +0000

    Home enabled on TwitterActivity

--- src/co/uk/socialticker/ticker/TwitterActivity.java
+++ src/co/uk/socialticker/ticker/TwitterActivity.java
@@ -21,6 +21,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.text.Html;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -90,6 +91,7 @@ public class TwitterActivity extends ActionBarActivity {
         //        android.R.color.transparent));
         actionBar.setTitle(getString(R.string.twitter));
         actionBar.setSubtitle(getString(R.string.app_name));
+        actionBar.setDisplayHomeAsUpEnabled(true);
          
         cd = new ConnectionDetector(getApplicationContext());
  
@@ -377,6 +379,22 @@ public class TwitterActivity extends ActionBarActivity {
 
         }
     }
+    
+    /* on menu click */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch (item.getItemId()) {
+	      // action with ID action_refresh was selected
+	    	case android.R.id.home:
+	    		this.finish();
+	    		return true;
+	
+	    	default:
+	    		break;
+	      }
+
+    	return true;
+    } 
  
 }
         
